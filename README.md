264f8521e208fb99803c7bc41af25aee237cdfdb
CamronG <git@siege.anonaddy.com>
2022-08-30 12:00:34 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+A safe drop-in replacement for Flutter's `ValueNotifier`, that makes
+`notifyListeners()` a no-op, rather than an error, after its disposal.
+
+Uses [safe_change_notifier](https://github.com/ubuntu-flutter-community/safe_change_notifier)
---
+++ lib/safe_value_notifier.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:safe_change_notifier/safe_change_notifier.dart';
+
+/// A [ChangeNotifier] that holds a single value.
+///
+/// When [value] is replaced with something that is not equal to the old
+/// value as evaluated by the equality operator ==, this class notifies its
+/// listeners.
+class SafeValueNotifier<T> extends SafeChangeNotifier implements ValueListenable<T> {
+  /// Creates a [ChangeNotifier] that wraps this value.
+  SafeValueNotifier(this._value);
+
+  /// The current value stored in this notifier.
+  ///
+  /// When the value is replaced with something that is not equal to the old
+  /// value as evaluated by the equality operator ==, this class notifies its
+  /// listeners.
+  @override
+  T get value => _value;
+  T _value;
+  set value(T newValue) {
+    if (_value == newValue) {
+      return;
+    }
+    _value = newValue;
+    notifyListeners();
+  }
+
+  @override
+  String toString() => '${describeIdentity(this)}($value)';
+}
